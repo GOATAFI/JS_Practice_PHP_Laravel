052ff3ca26c974838ac1bf9e3dc101f01b123be3
GOATAFI <projectexecutiveofpm@gmail.com>
2024-06-09 22:44:01 +0600

    for of loop

--- ES6 Practice/main.js
+++ ES6 Practice/main.js
@@ -1,7 +1,39 @@
 
-"use strict"; 
+// "use strict"; 
 // "use strict use na korle error throw korbe na even if name1 declare kora nai"
 myName();
 function myName() {
   name1 = "Nahid Parvez Mafi";
+}
+
+// ------------ Spread Operator ------------
+let poorCountry = ['Bangladesh','Sri Lanka'];
+let richCountry = ['USA','Canada','Australia'];
+
+let mixCountry = [...poorCountry , ...richCountry]; //array er element gula spread operator diye ber kore ana jay
+console.log(mixCountry);
+
+
+// ---------- Rest Parameter ----------
+function calculation(...numbers){
+    let sum = 0;
+    for (let i of numbers){
+        sum += i;
+    }
+    console.log(sum);
+}
+
+calculation(1,2,3,4,5);
+
+// ----------- Variable Hoisting ----------
+// age assign , pore declaration 
+name2 = "Nahid Parvez Mafi 2";
+console.log(name2);
+var name2; //eta abar let diye korle hoy na , var e lage
+
+//---------- for of loop -----------
+// array te loop chalanor jonno for of loop best
+let country = ['Dhaka','Barishal','Rajshahi'];
+for (jhela of country){
+    console.log(jhela);
 }
